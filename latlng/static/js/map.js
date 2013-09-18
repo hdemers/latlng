5d90f13799b7df05222c38d1095b4ea994230055
Hugues Demers <hdemers@gmail.com>
2013-09-18 19:19:54 -0400

    Use Knockout's mapping to update peers.
    Use a Knockout observable array for managing the list of peers. Updating the
    map is done through observable subscriptions as will be other view elements.

--- latlng/static/js/map.js
+++ latlng/static/js/map.js
@@ -1,6 +1,6 @@
 /* Author: Hugues Demers
  * Copyrights 2013
- */
+*/
 
 /*global appConfig:false*/
 define([
@@ -9,14 +9,23 @@ define([
   "leaflet",
 ],
 function ($, _, L) {
-  var exports = {}, map, mapElement, markers = {};
+  var exports = {}, map, mapElement, markers = {},
+    setViewAll, heartbeatTimeoutId;
 
-  exports.initialize = function (element) {
+  exports.initialize = function (element, options) {
     console.log("Initializing map.");
-    var cloudmade;
+    var cloudmade,
+      defaults = {
+        locate: true,
+        heartbeat: true
+      }, opts = _.defaults(options || {}, defaults);
 
     mapElement = element;
     map = L.map(mapElement);
+
+    exports.locate = function () {
+      map.locate();
+    };
 
     cloudmade = L.tileLayer(
       'http://{s}.tile.cloudmade.com/' + appConfig.cloudmadeApiKey +
@@ -26,36 +35,51 @@ function ($, _, L) {
 
     map.setView([35, -50], 3);
 
-    map.locate({
-      watch: true,
-      locate: true,
-      //setView: true,
-      enableHighAccuracy: true
-    });
+    if (opts.locate) {
+      map.locate({
+        watch: true,
+        enableHighAccuracy: true
+      });
+    }
 
-    exports.mark = function (location, ident, iconUrl) {
-      var marker, icon = L.icon({iconUrl: iconUrl, iconSize: [25, 25]});
+    // When on a desktop, calling map.locate with watch = true, will fire only
+    // one locationfound event. Here, we set up a timeout that will call
+    // map.locate (watch = false) to force a position update from the cache
+    // (maximumAge: Infinity).
+    if (opts.heartbeat) {
+      map.on('locationfound', function () {
+        clearTimeout(heartbeatTimeoutId);
+        heartbeatTimeoutId = setTimeout(function () {
+          console.log("Heartbeating");
+          map.locate({maximumAge: Infinity});
+        }, 8000);
+      });
+      map.locate({watch: false, enableHighAccuracy: true});
+    }
+  };
 
-      if (_.has(markers, ident)) {
-        markers[ident].setLatLng(location.latlng);
-        markers[ident].setIcon(icon);
-        //marker.setAccuracy(location.accuracy);
-      }
-      else {
-        marker = L.marker(location.latlng, {icon: icon});
-        markers[ident] = marker;
-        marker.addTo(map);
-        //marker.setAccuracy(location.accuracy);
-        exports.setViewAll();
-      }
-    };
+  exports.mark = function (location, ident, iconUrl) {
+    var marker, icon = L.icon({iconUrl: iconUrl, iconSize: [25, 25]});
+
+    if (_.has(markers, ident)) {
+      markers[ident].setLatLng(location.latlng);
+      markers[ident].setIcon(icon);
+      //marker.setAccuracy(location.accuracy);
+    }
+    else {
+      marker = L.marker(location.latlng, {icon: icon});
+      markers[ident] = marker;
+      marker.addTo(map);
+      //marker.setAccuracy(location.accuracy);
+      setViewAll();
+    }
   };
 
   exports.on = function (eventType, callback) {
     map.on(eventType, callback);
   };
 
-  exports.setViewAll = function () {
+  setViewAll = function () {
     map.fitBounds(L.featureGroup(_.values(markers)).getBounds());
   };
 
--- latlng/static/js/viewmodel.js
+++ latlng/static/js/viewmodel.js
@@ -1,11 +1,45 @@
 /* Author: Hugues Demers
  * Copyrights 2013
  */
-define(["knockout"],
-function (ko) {
-  var exports = {
-    senderEmail: ko.observable(),
-    recipientEmail: ko.observable()
+define(["knockout", "underscore", "mapping", "map"],
+function (ko, _, mapping, map) {
+  var peerMapping = {
+    key: function (item) {
+      return ko.utils.unwrapObservable(item.ident);
+    },
+    create: function (options) {
+      var peer = mapping.fromJS(options.data),
+        updateMap = function () { exports.updateMap(peer); };
+
+      peer.email.subscribe(updateMap);
+      peer.location.latlng.subscribe(updateMap);
+      return peer;
+    }
+  }, exports = {
+    senderEmail: ko.observable('hdemers@gmail.com'),
+    recipientEmail: ko.observable('hugues.demers@gmail.com'),
+    peers: mapping.fromJS([], peerMapping)
   };
+
+
+  // A peer:
+  //  { 
+  //    ident,
+  //    location: {accuracy, latlng},
+  //    email
+  //  }
+
+  exports.update = function (ident, data) {
+    var index;
+    data.ident = ident;
+    index = exports.peers.mappedIndexOf(data);
+    if (index !== -1) {
+      mapping.fromJS(data, peerMapping, exports.peers()[index]);
+    }
+    else {
+      exports.peers.mappedCreate(data);
+    }
+  };
+
   return exports;
 });
